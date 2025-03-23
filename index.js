00919ff8df810326f28cb534d0fccd6c49f4d9dc
Polb18 <159519859+Polb18@users.noreply.github.com>
2025-03-23 22:45:02 +0000

    historia8

---
+++ index.js
@@ -0,0 +1,261 @@
+let bd = [
+    {
+        tituloNoticia: `L'Encant d'Acampar al Costat del llac`,
+        img: `../../imagenes/news1.png`,
+        introduccion: `<p>
+            Hi ha poques experiències a la vida tan tranquil·les i refrescants com <strong> al costat d'un llac</strong>. 
+            La combinació de la natura en el seu estat més pur amb la calma de les aigües és simplement irresistible.
+            Quan arribes al càmping i plantes la tenda a prop de la riba, la primera cosa que notes és la pau que envolta l'entorn. 
+            El lleuger onatge de l'aigua i el suau xiuxiueig del vent entre els arbres et conviden a desconnectar del món i gaudir de cada moment.
+        </p>`,
+        Noticia: 
+        `
+         <p>
+            Hi ha poques experiències a la vida tan tranquil·les i refrescants com <strong> al costat d'un llac</strong>. 
+            La combinació de la natura en el seu estat més pur amb la calma de les aigües és simplement irresistible.
+            Quan arribes al càmping i plantes la tenda a prop de la riba, la primera cosa que notes és la pau que envolta l'entorn. 
+            El lleuger onatge de l'aigua i el suau xiuxiueig del vent entre els arbres et conviden a desconnectar del món i gaudir de cada moment.
+        </p>
+
+        
+        <div class="citas">
+            <ul>
+                <em>
+                    "La proximitat de l'aigua aporta una pau que no es troba en cap altre lloc. Despertar amb la vista del llac és una experiència que tots haurien de viure almenys una vegada."
+                </em>
+            </ul>
+        </div>
+
+        <p>
+            Acampar al costat d'un llac no només ofereix unes vistes espectaculars, sinó que també proporciona una multitud d'activitats per gaudir. 
+            Pots començar el dia amb una refrescant banyada a primera hora del matí o fer una passejada amb caiac mentre el sol surt per l'horitzó. 
+            La <strong>pesca</strong> és una altra activitat popular, on pots provar la teva sort i capturar el sopar del dia. 
+            A la nit, encén una foguera i gaudeix d'un sopar a l'aire lliure mentre el sol es pon sobre les aigües tranquil·les del llac.
+        </p>
+
+        <p>
+            <em>Acampar al costat d'un llac també és ideal per a les famílies</em>. Els nens poden passar hores jugant a la vora de l'aigua, construint castells de sorra o buscant pedres especials. 
+            La proximitat a l'aigua també afegeix una dimensió única a l'experiència de càmping, fent que sigui més relaxant i oferint moltes oportunitats per connectar amb la natura.
+        </p>
+        
+        <p>
+            Per aquells que busquen una experiència de càmping autèntica però còmoda, els càmpings a la vora del llac són l'elecció perfecta. 
+            La combinació de la <strong> bellesa natural </strong> amb la serenitat de l'aigua crea un entorn incomparable, ideal per a aquells que volen desconnectar i gaudir de la natura en estat pur.
+        </p>
+        `
+    },
+    {
+        tituloNoticia: `Glamping: Una Nova Manera de Gaudir de la Natura `,
+        img: `../../imagenes/news2.png`,
+        introduccion: `<p>
+            El <strong>glamping</strong> (glamour + camping) és la tendència més recent en el món del turisme de natura, oferint una combinació única entre el luxe d'un hotel i l'experiència d'acampar a l'aire lliure. Aquesta forma d'acampada permet gaudir de tots els avantatges de la natura, sense renunciar a les comoditats modernes.
+        </p>`,
+        Noticia:
+        `
+                <p>
+            El <strong>glamping</strong> (glamour + camping) és la tendència més recent en el món del turisme de natura, oferint una combinació única entre el luxe d'un hotel i l'experiència d'acampar a l'aire lliure. Aquesta forma d'acampada permet gaudir de tots els avantatges de la natura, sense renunciar a les comoditats modernes.
+        </p>
+
+        <p>
+            Si sempre t'ha atret l'idea d'acampar però t'intimiden les incomoditats associades amb el càmping tradicional, el glamping és la solució perfecta. Les tendes de glamping solen estar equipades amb <em><strong>llits còmodes</strong></em>, electricitat, banys privats i fins i tot cuines equipades. Això significa que pots gaudir de les estrelles a la nit i despertar-te amb els sons de la natura, però sense sacrificar el confort.
+        </p>
+
+        <p>
+            Una altra de les avantatges del glamping és la seva ubicació. Moltes destinacions de glamping estan situades en llocs espectaculars, com parcs nacionals, muntanyes, deserts i, per descomptat, a la vora de llacs. Aquestes ubicacions privilegiades permeten als hostes gaudir de la <strong>bellesa natural</strong>sense la necessitat d'equipament complex o experiència prèvia en càmping.
+        </p>
+
+        <p>
+            <div class="citas">
+                <ul>
+                    <em>
+                        "El glamping ofereix una manera més sostenible de viatjar, utilitzant materials ecològics i dissenys amb un impacte mínim sobre el medi ambient."    
+                    </em>
+                </ul>
+            </div>
+        </p>
+
+        <p>
+            En resum, el glamping és la manera ideal de combinar la comoditat amb l'aventura. És perfecte per a parelles que busquen una escapada romàntica, famílies que volen experimentar la natura sense renunciar al confort, o per a qualsevol que desitgi una experiència única a l'aire lliure sense les complicacions del càmping tradicional.
+        </p>
+        `
+    },
+    {
+        tituloNoticia: ` Els Millors Destins per Gaudir del Glamping amb Estil `,
+        img: `../../imagenes/news3.png`,
+        introduccion: `<p>
+            El <stron> glamping </stron> ha esdevingut una de les maneres més populars de connectar amb la natura sense renunciar a les comoditats modernes. Però no tots els destins de glamping són iguals; hi ha llocs que porten aquesta experiència al següent nivell, oferint una fusió perfecta entre luxe i natura. Aquí et presentem alguns dels millors destins per gaudir del glamping amb estil.
+        </p>`,
+        Noticia:
+        `
+            <p>
+            El <stron> glamping </stron> ha esdevingut una de les maneres més populars de connectar amb la natura sense renunciar a les comoditats modernes. Però no tots els destins de glamping són iguals; hi ha llocs que porten aquesta experiència al següent nivell, oferint una fusió perfecta entre luxe i natura. Aquí et presentem alguns dels millors destins per gaudir del glamping amb estil.
+        </p>
+
+        <ul>
+            <li>
+                <strong>El Montseny, Catalunya:</strong> Conegut per les seves muntanyes verdes i boscos frondosos, el Montseny ofereix una experiència de glamping incomparable. Envoltats de natura exuberant, les tendes de glamping en aquesta zona estan equipades amb totes les comoditats, des de llits king-size fins a banyeres exteriors amb vistes espectaculars. És el lloc perfecte per desconnectar i gaudir de la tranquil·litat.
+            </li>
+
+            <li>
+                <strong>El Delta de l’Ebre:</strong>Aquest paradís natural és ideal per als amants de la biodiversitat. Aquí, el glamping permet gaudir de la rica fauna i flora del Delta sense perdre confort. Pots passar el dia explorant els canals amb bicicleta o en caiac, i tornar a la teva tenda de glamping per gaudir d'un sopar preparat amb productes locals mentre el sol es pon sobre les aigües tranquil·les.
+            </li>
+
+            <li>
+                <strong>Els Pirineus:</strong>Si ets un amant de les muntanyes, els Pirineus són una destinació imprescindible. Aquí trobaràs opcions de glamping que combinen la proximitat a rutes de senderisme impressionants amb allotjaments de luxe. Després d’un dia d’aventures, podràs relaxar-te en una tenda equipada amb totes les comoditats modernes, incloent calefacció per les nits fredes de muntanya.
+            </li>
+
+            <li>
+                <strong>La Costa Brava:</strong> Per aquells que volen gaudir del mar, la Costa Brava ofereix una experiència de glamping única. Amb tendes situades a pocs passos de platges de sorra fina, podràs gaudir d'unes vacances al costat del mar amb tot el luxe d’un hotel de cinc estrelles. Des de veure la sortida del sol des de la teva tenda fins a gaudir de menjars gourmet preparats amb ingredients locals, el glamping a la Costa Brava és una experiència inoblidable.
+            </li>
+        </ul>
+        <p>
+            El glamping és molt més que una simple tendència; és una nova manera de viatjar que combina l'amor per la natura amb les comoditats de la vida moderna. Si busques una experiència de vacances que ofereixi el millor dels dos mons, aquests destins de glamping amb estil són l'opció perfecta.
+        </p>
+        `
+    },
+    {
+        tituloNoticia: `<h1 class="titolArticle"> Els Beneficis de l'Ecoturisme per al Medi Ambient </h1>`,
+        img: `../../imagenes/img4.jpg`,
+        introduccion:`<p>
+            L'<strong>ecoturisme</strong> s'ha convertit en una de les formes de turisme més populars i respectuoses amb el medi ambient. Aquesta pràctica no només permet als viatgers gaudir de la natura, sinó que també contribueix a la conservació dels ecosistemes i al benestar de les comunitats locals.
+        </p>`,
+        Noticia:
+        `
+            <p>
+            L'<strong>ecoturisme</strong> s'ha convertit en una de les formes de turisme més populars i respectuoses amb el medi ambient. Aquesta pràctica no només permet als viatgers gaudir de la natura, sinó que també contribueix a la conservació dels ecosistemes i al benestar de les comunitats locals.
+        </p>
+
+        <p>
+            Un dels principals beneficis de l'ecoturisme és la seva capacitat per promoure la <strong>conservació de la biodiversitat</strong>. En visitar àrees protegides o parcs naturals, els turistes ajuden a finançar projectes de conservació i a mantenir aquests espais en bon estat. A més, l'ecoturisme fomenta la conscienciació ambiental, ja que els viatgers aprenen sobre la importància de protegir el medi ambient i les espècies que hi habiten.
+        </p>
+
+        <p>
+            <div class="citas">
+                <ul>
+                    <em>
+                        "L'ecoturisme no és només una manera de viatjar; és una filosofia que busca minimitzar l'impacte ambiental i maximitzar els beneficis per a les comunitats locals."
+                    </em>
+                </ul>
+            </div>
+        </p>
+
+        <p>
+            Una altra avantatge de l'ecoturisme és el seu impacte positiu en les comunitats locals. En molts casos, aquest tipus de turisme genera ingressos per a les poblacions indígenes o rurals, oferint-los oportunitats laborals i millorant la seva qualitat de vida. Així, l'ecoturisme no només protegeix el medi ambient, sinó que també contribueix al desenvolupament sostenible de les regions visitades.
+        </p>
+
+        <p>
+            En resum, l'ecoturisme és una manera responsable i enriquidora de viatjar que beneficia tant el medi ambient com les comunitats locals. Si busques una experiència de viatge que tingui un impacte positiu, l'ecoturisme és sens dubte una excel·lent opció.
+        </p>
+        `
+    },
+    {
+        tituloNoticia: `<h1 class="titolArticle"> Rutes de Senderisme per Descobrir la Catalunya Menys Coneguda</h1>`,
+        img: `../../imagenes/img5.jpg`,
+        introduccion: `<p>
+            Catalunya és una regió plena de bellesa natural, amb paisatges que van des de les muntanyes dels Pirineus fins a les platges de la Costa Brava. Però més enllà dels llocs més coneguts, hi ha una infinitat de rutes de senderisme que et permeten descobrir la Catalunya més autèntica i menys turística.
+        </p>`,
+        Noticia:
+        `
+            <p>
+            Catalunya és una regió plena de bellesa natural, amb paisatges que van des de les muntanyes dels Pirineus fins a les platges de la Costa Brava. Però més enllà dels llocs més coneguts, hi ha una infinitat de rutes de senderisme que et permeten descobrir la Catalunya més autèntica i menys turística.
+        </p>
+
+        <ul>
+            <li>
+                <strong>La Ruta dels Volcans a la Garrotxa:</strong> Aquesta ruta et porta a través d'un paisatge únic, format per volcans extints i boscos densos. És una experiència ideal per als amants de la geologia i la natura, amb vistes espectaculars i una sensació de tranquil·litat incomparable.
+            </li>
+
+            <li>
+                <strong>El Camí de Ronda a la Costa Brava:</strong> Aquesta ruta costanera ofereix vistes impressionants del mar Mediterrani, amb cales amagades i pobles pesquers que semblen sortits d'un conte. És perfecta per a aquells que volen combinar senderisme amb banyades refrescants.
+            </li>
+
+            <li>
+                <strong>El Parc Natural del Cadí-Moixeró:</strong> Situat als Pre-Pirineus, aquest parc ofereix rutes per a tots els nivells, des de passejades tranquiles fins a ascensions desafiantes. Les vistes de les muntanyes i la riquesa de la fauna i flora fan d'aquest lloc un paradís per als excursionistes.
+            </li>
+
+            <li>
+                <strong>La Ruta del Cister:</strong> Aquesta ruta combina història i natura, portant-te a través de monestirs cistercencs i paisatges rurals. És una manera perfecta de descobrir el patrimoni cultural de Catalunya mentre gaudeixes d'una caminada tranquil·la.
+            </li>
+        </ul>
+
+        <p>
+            Aquestes rutes són només una mostra de la gran varietat d'opcions que ofereix Catalunya per als amants del senderisme. Ja sigui que prefereixis les muntanyes, la costa o els paisatges rurals, hi ha una ruta per a tu. Així que posa't les botes i descobreix la Catalunya menys coneguda!
+        </p>
+        `
+    }
+];
+
+const url = window.location.href;
+ 
+const parametros = new URLSearchParams(window.location.search);
+let id = parametros.get('id');
+
+if (id != "noticiasNews" && id!= 0 && id!= 1 && id!= 2 && id!=3 && id!= 4){
+    
+    const botonNews = document.querySelector("#noticiasNews");
+    
+    botonNews.addEventListener("click",function(e){
+        e.target;  
+        window.location.href =`Webs/News/News.html?id=${e.target.id}`;
+        
+    })
+    
+}else if(id == "noticiasNews"){
+    
+    let div = document.querySelector("#noticias")
+    let articulos  = ""
+    for(let i = 0; i < bd.length; i++){
+    
+            articulos += ` <div class="contenidoNews"> <img  class="imgDetalle" id="${i+1}" src="${bd[i].img}">  <div> <h1 class="titolArticle"> ${bd[i].tituloNoticia} </h1> 
+            ${bd[i].introduccion}
+            <p class="noticia" id="${i+1}"> Read </p>
+            </div>
+            
+            </div>
+             `
+    }
+    div.innerHTML = articulos; 
+}
+
+const divIndex = document.querySelectorAll(".new");
+
+for(let i = 0; i < divIndex.length; i++){
+    mensajePrincipal = `<img src="${bd[i].img}" alt="Imagen sobre la noticia 1" id="${i+1}" class="imgDetalle">
+    <p>${bd[i].tituloNoticia}</p>
+    <a><p id="${i+1}" class="noticia">Read</p></a>`;
+
+    divIndex[i].innerHTML = mensajePrincipal;
+}
+
+
+const read = document.querySelectorAll(".noticia");
+for(let i = 0; i < read.length; i++){
+    read[i].addEventListener("click",function(e){
+        let idDetalle = e.currentTarget.id // RECOJO CORRECTAMENTE EL ID
+        console.log("EL NUMERO ES",idDetalle)
+        window.location.href = `https://cuddly-sniffle-7vpv6xwj56w6hxr6x-5503.app.github.dev/Webs/Others/detallNoticia.html?id=${idDetalle-1}`; 
+    })    
+}
+
+const imgDetalle = document.querySelectorAll(".imgDetalle");
+
+for(let i = 0; i < imgDetalle.length; i++){
+    imgDetalle[i].addEventListener("click",function(e){
+        let idDetalle = e.currentTarget.id // RECOJO CORRECTAMENTE EL ID
+        console.log("EL NUMERO ES",idDetalle)
+        window.location.href = `https://cuddly-sniffle-7vpv6xwj56w6hxr6x-5503.app.github.dev/Webs/Others/detallNoticia.html?id=${idDetalle-1}`; 
+    })    
+}
+
+if(id == 0 || id == 1 || id == 2 || id== 3 || id==4){
+    const contenidoDetalles = document.querySelector("#detalle"); 
+    contenidoDetalles.innerHTML = ` <div class="contenidoNews">   <h1 class="titolArticle"> ${bd[id].tituloNoticia} </h1> 
+            <div class="imgDetalle">         
+            <img src="${bd[id].img}">
+            <div class="texto"> 
+                ${bd[id].Noticia}
+            </div>
+            </div>
+            </div>
+            `
+}
